b8dec6acefd834b8697a27c6c363de0669e72e5f
Volodya <example@gmail.com>
2022-08-03 18:17:30 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let incr = 10,
+    decr = 10;
+    
+// ++incr;
+// decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+   
+
+
+
+const isChecked = false,
+      isClose = false;
+
+console.log(isChecked || !isClose);
